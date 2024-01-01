84fa53bca76134b0baa1ec3ee341638bcc9aedc3
Rafael Francisco Serrano Urtecho <31552767+phaetton@users.noreply.github.com>
2024-01-01 02:23:17 -0800

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { PeliculaComponent } from './pages/pelicula/pelicula.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'buscar/:texto',component:BuscarComponent},
+  {path:'pelicula/:id',component:PeliculaComponent},
+  {path:'',pathMatch:'full',redirectTo:'/home'},
+  {path:'**',pathMatch:'full',redirectTo:'/home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
